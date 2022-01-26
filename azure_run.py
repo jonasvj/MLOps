c0c31adf1cef07e1a1bcf6cb2468821f23e78272
jonasvj <jonas_vez@hotmail.com>
2022-01-26 12:43:37 +0100

    Update

--- azure_run.py
+++ azure_run.py
@@ -4,16 +4,14 @@ from azureml.core import Environment
 from azureml.core import ScriptRunConfig
 from azureml.core.conda_dependencies import CondaDependencies
 from wandb_api_key import WANDB_API_KEY
-#from azureml.widgets import RunDetails
 
 if __name__ == '__main__':
     # Pip packages
     with open('requirements.txt', 'r') as f:
         pip_packages = [
             pkg.strip().lstrip('-e ') for pkg in f if not pkg.startswith('#')]
-    pip_packages.remove('git+https://github.com/jonasvj/MLOps@d8af0044cc5bcd98a81e4128ac13b169cd619b97#egg=src')
-    print(pip_packages)
-    # Setup workspace and environment
+
+    # Workspace and environment
     ws = Workspace.from_config()
    
     env = Environment(name='mnist-env')
@@ -23,10 +21,7 @@ if __name__ == '__main__':
         python_version='3.7.2',
         conda_packages=['pip==21.1.2'],
         pip_packages=pip_packages)
-    conda_dep.set_pip_option('-e git+https://github.com/jonasvj/MLOps@d8af0044cc5bcd98a81e4128ac13b169cd619b97#egg=src')
-    #conda_dep.set_pip_option('-e .')
     env.python.conda_dependencies = conda_dep
-    #env.python.user_managed_dependencies = True
 
     # Create a script config
     script_config = ScriptRunConfig(
@@ -39,5 +34,4 @@ if __name__ == '__main__':
     experiment = Experiment(workspace=ws, name='mnist-test')
     run = experiment.submit(script_config)
 
-    #RunDetails(run).show()
     run.wait_for_completion()
